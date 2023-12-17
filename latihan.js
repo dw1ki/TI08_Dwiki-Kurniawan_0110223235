005a46120bf54acde6b927a28bb23735c878168a
dw1ki <dwikikurniawanreal@gmail.com>
2023-12-17 23:51:44 +0700

    add week 11

---
+++ latihan.js
@@ -0,0 +1,47 @@
+let nama = "Dwiki Kurniawan"
+console.log(nama)
+
+const firstName = "Dwiki"
+const lastname = "Kurniawan"
+console.log(firstName  + " " + lastname)
+
+const gender = "LK";
+console.log(typeof gender)
+
+const age = 21
+console.log(age)
+console.log(typeof age)
+
+const isMarried = false;
+console.log(isMarried)
+console.log(typeof isMarried)
+
+const x = 9
+const y = 10
+
+const z = x + y
+
+const k = y - x
+
+const l = x * y
+
+const m = x / y
+
+const n = y % 2
+
+console.log(z)
+console.log(k)
+console.log(l)
+console.log(m)
+console.log(n)
+
+function salam() {
+    alert("Assalamualaikum")
+}
+
+salam();
+
+function isTrue() {
+    let city = "Depok"
+    
+}
